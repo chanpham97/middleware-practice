d2f1ba9b9f961d474ebf6fc39de101f1c3d582cb
Chan Pham <iamchanpham@gmail.com>
2024-12-10 20:28:42 +0000

    removed quotes from keys

--- index.js
+++ index.js
@@ -2,62 +2,62 @@ const express = require('express')
 const app = express()
 
 let spaceData = {
-    "planets": [
+    planets: [
         {
-            "name": "Mercury",
-            "distance_from_sun_km": 57900000,
-            "diameter_km": 4879,
-            "moons": 0
+            name: "Mercury",
+            distance_from_sun_km: 57900000,
+            diameter_km: 4879,
+            moons: 0
         },
         {
-            "name": "Venus",
-            "distance_from_sun_km": 108200000,
-            "diameter_km": 12104,
-            "moons": 0
+            name: "Venus",
+            distance_from_sun_km: 108200000,
+            diameter_km: 12104,
+            moons: 0
         },
         {
-            "name": "Earth",
-            "distance_from_sun_km": 149600000,
-            "diameter_km": 12742,
-            "moons": 1
+            name: "Earth",
+            distance_from_sun_km: 149600000,
+            diameter_km: 12742,
+            moons: 1
         },
         {
-            "name": "Mars",
-            "distance_from_sun_km": 227900000,
-            "diameter_km": 6779,
-            "moons": 2
+            name: "Mars",
+            distance_from_sun_km: 227900000,
+            diameter_km: 6779,
+            moons: 2
         },
         {
-            "name": "Jupiter",
-            "distance_from_sun_km": 778500000,
-            "diameter_km": 139820,
-            "moons": 95
+            name: "Jupiter",
+            distance_from_sun_km: 778500000,
+            diameter_km: 139820,
+            moons: 95
         },
         {
-            "name": "Saturn",
-            "distance_from_sun_km": 1433000000,
-            "diameter_km": 116460,
-            "moons": 146
+            name: "Saturn",
+            distance_from_sun_km: 1433000000,
+            diameter_km: 116460,
+            moons: 146
         },
         {
-            "name": "Uranus",
-            "distance_from_sun_km": 2871000000,
-            "diameter_km": 50724,
-            "moons": 27
+            name: "Uranus",
+            distance_from_sun_km: 2871000000,
+            diameter_km: 50724,
+            moons: 27
         },
         {
-            "name": "Neptune",
-            "distance_from_sun_km": 4495000000,
-            "diameter_km": 49244,
-            "moons": 14
+            name: "Neptune",
+            distance_from_sun_km: 4495000000,
+            diameter_km: 49244,
+            moons: 14
         }
     ],
-    "astronauts_in_space": {
-        "total": 10,
-        "locations": [
+    astronauts_in_space: {
+        total: 10,
+        locations: [
             {
-                "station": "International Space Station",
-                "astronauts": [
+                station: "International Space Station",
+                astronauts: [
                     "Sunita Williams",
                     "Barry Wilmore",
                     "Donald Pettit",
@@ -68,8 +68,8 @@ let spaceData = {
                 ]
             },
             {
-                "station": "Tiangong Space Station",
-                "astronauts": [
+                station: "Tiangong Space Station",
+                astronauts: [
                     "Cai Xuzhe",
                     "Song Lingdong",
                     "Wang Haoze"
@@ -78,6 +78,7 @@ let spaceData = {
         ]
     }
 }
+
 
 // INSTRUCTIONS: Set up the code that does the following
 // - console logs the method and URL for every request
